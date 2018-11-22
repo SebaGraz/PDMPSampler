92274f5dbe34d149fb8c858b56d361fff77c0fbb
SebaGraz <43820633+SebaGraz@users.noreply.github.com>
2018-11-22 13:56:26 +0100

    clean application

--- Examples/Application.jl
+++ Examples/Application.jl
@@ -1,5 +1,10 @@
-#making the module soon
+#push!(LOAD_PATH, "C:\\Users\\sebas\\.julia\\dev\\ZigZag\\src")
 using ZigZag
-N= MultiNormal([1.0, 1.0],[1.0 0.0; 0.0 1.0])
-a = ZigZag(N,1.0,100.0)
-# I have to figure out how to plot the coordinate
+using Plots
+ξ0 = zeros(2)
+N= MultiNormal([1.0, 1.0],[1.0 0.5; 0.5 1.0])
+M = Normal(1.0, 1.0)
+a = ZigZagGaussian(N, ξ0 , 100.0)
+b = ZigZagGaussian(M, -10.0, 100.0)
+
+plot(b[1],b[2])
