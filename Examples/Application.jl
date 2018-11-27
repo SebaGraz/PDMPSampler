86851f6cd505d8a71d7465337510dfda6c65a928
SebaGraz <43820633+SebaGraz@users.noreply.github.com>
2018-11-27 10:19:50 +0100

    Multi dimensional case fixed (still raw version though).

--- Examples/Application.jl
+++ Examples/Application.jl
@@ -1,15 +1,17 @@
 push!(LOAD_PATH, "C:\\Users\\sebas\\.julia\\dev\\ZigZag\\src")
 using ZigZag
 using Plots
+using Statistics, Random, LinearAlgebra
 ξ0 = zeros(2)
-N= MultiNormal([5.0, 1.0],[10.0 0.5; 0.5 1.0])
 M = Normal(5.0, 1.0)
-a = ZigZagGaussian(N, ξ0 , 1000.0)
 b = ZigZagGaussian(M, -10.0, 1000.0)
 #Plot one dimensional Gaussian
 plot(b[1],b[2])
+inv(b[1][end]-b[1][1])*sum(diff(b[1]).* [(b[2][i]+b[2][i-1])/2 for i in 2:length(b[2])])
 
 #Plot 2 coordinates of d-dimensional Gaussian
+N= MultiNormal([0.0, 0.0],[10.0 -9.0; -9.0 10.0])
+a = ZigZagGaussian(N, ξ0 , 10000.0)
 d = length(a[2])
 x = zeros(d)
 y = zeros(d)
--- src/ZigZag.jl
+++ src/ZigZag.jl
@@ -27,11 +27,41 @@ module ZigZag
         t = 0.0 ; θ = ones(d); ξ = ξ0
         Ξ = [ξ]
         Γ = [t]
+        τ = zeros(d)
+        V = inv(N.Σ)
         while t<T
-            τ = sqrt.(-2*N.Σ*log.(rand(d)) + max.(0 ,θ.*(ξ - N.μ)).^2) - θ.*(ξ - N.μ)
-            τ, i0 = findmin(τ)
-            t = t + τ
-            ξ = ξ + θ.*τ
+            b = θ.*(V*θ)
+            a = θ.*(V*ξ)
+            println(a)
+            for i in 1:2
+                if b[i] > 0
+                    if a[i] < 0
+                        τ[i] = sqrt(-log(rand())*2.0/b[i]) - a[i]/b[i]
+                    else #a[i]>0
+                        τ[i] = sqrt((a[i]/b[i])^2 - log(rand())*2.0/b[i]) - a[i]/b[i]
+                    end
+                elseif  b[i] == 0
+                    if a[i] > 0
+                        τ[i] = -log(rand())/a[i]
+                    else #a[i] <= 0
+                        τ[i] = Inf
+                    end
+                else #b[i] < 0
+                    if a[i] <= 0
+                        τ[i] = Inf
+                    else
+                        u = rand()
+                        if -log(u) <= -a[i]^2/b[i] + a[i]^2/(2*b[i])
+                            τ[i] = - sqrt((a[i]/b[i])^2 - log(u)*2.0/b[i]) - a[i]/b[i]
+                        else
+                            τ[i] = Inf
+                        end
+                    end
+                end
+            end
+            τ1, i0 = findmin(τ)
+            t = t + τ1
+            ξ = ξ + θ*τ1
             push!(Ξ, ξ)
             push!(Γ, t)
             θ[i0] = -θ[i0]
